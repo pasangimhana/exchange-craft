1d45c0c67d9c490e54c452e76ebf2b7a8fc541a0
Pasan Gimhana <85233319+pasangimhana@users.noreply.github.com>
2024-01-10 17:37:24 +0000

    Changes

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <chrono>
+#include <filesystem>
+#include <stdlib.h>
 
 #include "ExchangeCore.h"
 
 int main(int argc, char **argv)
 {
     auto start = std::chrono::high_resolution_clock::now();
+
 
     ExchangeCore exchangeCore;
     exchangeCore.parseCSV("/home/phaser/personal-projects/exchange-craft/data.csv");
